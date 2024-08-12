b420c16cfc30dcd70fa2be9a3d6f7ec8cfd4e41d
Zurionek <marcin.a.zurek@icloud.com>
2024-08-13 00:16:52 +0200

    Write a code for a mouse moving bot

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 """Simple bot to move a mouse cursor randomly every 2 seconds"""
+import pyautogui as pag
+import random
+import time
+
+while True:
+    x = random.randint(600,700)
+    y = random.randint(200, 600)
+    pag.moveTo(x, y, 0.5)
+    time.sleep(2)
